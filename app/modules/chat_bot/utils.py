c00ecb32a96ffe40eedadc44f637f2cbf67ed5e6
Dmitrii <wonderrfau1t@gmail.com>
2026-04-18 17:34:18 +0700

    added docker-compose file

--- app/modules/chat_bot/utils.py
+++ app/modules/chat_bot/utils.py
@@ -6,7 +6,7 @@ import redis
 from app.core.clients.vk_api import client
 from app.modules.analyzer.models import APIResponse
 
-redis_client = redis.Redis(host="localhost", port=6379, db=0)
+redis_client = redis.Redis(host="redis", port=6379, db=0)
 redis_client.flushdb()
 
 
