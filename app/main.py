38883397b31faeebfac72b4d34ac3717e5865022
Dmitrii <wonderrfau1t@gmail.com>
2026-06-18 20:44:13 +0700

    fix: update CORS allowed origins to include new VK app URL

--- app/main.py
+++ app/main.py
@@ -66,7 +66,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tunnel.wonderrfau1t.site", "https://vk.com", "https://vk.ru"],
+    allow_origins=["https://tunnel.wonderrfau1t.site", "https://vk.com", "https://vk.ru",
+                   "https://prod-app52612592-031eae25d36f.pages-ac.vk-apps.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
